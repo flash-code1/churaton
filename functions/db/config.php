39d026308d27afc3686e63096efbd0baa9b99b85
flash code <47109193+flash-code1@users.noreply.github.com>
2020-05-30 01:44:03 +0100

    man

--- functions/db/config.php
+++ functions/db/config.php
@@ -2,7 +2,7 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no pssword) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'sekanisy');
+define('DB_USERNAME', 'churaton_hotel2');
 define('DB_PASSWORD', '4r6WY#JP+rnl67');
 define('DB_CHARSET', 'utf8');
 define('DB_NAME', 'churaton_hotel');
