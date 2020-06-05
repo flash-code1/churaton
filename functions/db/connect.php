83bf23519c651b73195dd252613c315064122707
flash code <47109193+flash-code1@users.noreply.github.com>
2020-06-05 10:39:23 +0100

    meen

--- functions/db/connect.php
+++ functions/db/connect.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'sekanisy');
+define('DB_USERNAME', 'churaton_hotel2');
 define('DB_PASSWORD', '4r6WY#JP+rnl67');
 define('DB_CHARSET', 'utf8');
 define('DB_NAME', 'churaton');
